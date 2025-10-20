55681f290510cd602940b1d8b898cc62d69e7e6d
Yusuf Samiwala <yusufbs@hotmail.com>
2025-10-19 19:41:54 -0500

    added some components and created router links via menu

--- Angular_NgRx/src/app/app.routes.ts
+++ Angular_NgRx/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Counter } from './counter/counter';
+import { Courses } from './courses/courses';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'counter', component: Counter },
+  { path: 'courses', component: Courses },
+];
---
+++ Angular_NgRx/src/app/counter/counter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css'
+})
+export class Counter {
+
+}
