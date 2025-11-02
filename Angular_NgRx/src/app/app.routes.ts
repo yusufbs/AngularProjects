a5bc532b3771cde87ca25ac661a5d7cc553151a1
Yusuf Samiwala <yusufbs@hotmail.com>
2025-11-01 19:20:39 -0500

    lazy loading routes

--- Angular_NgRx/src/app/app.routes.ts
+++ Angular_NgRx/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { Counter } from './counter/counter';
-import { Courses } from './courses/courses';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'counter', component: Counter },
-  { path: 'courses', component: Courses },
+  { path: 'counter', loadChildren: () => import('./counter/counter.routes').then((m) => m.routes) },
+  { path: 'courses', loadChildren: () => import('./courses/courses.routes').then((m) => m.routes) },
 ];
---
+++ Angular_NgRx/src/app/courses/courses.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { Courses } from './courses';
+
+export const routes: Routes = [{ path: '', component: Courses }];
